0652faa31ab30ca9c4875f8939c193252fd17962
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2026-08-12 05:10:28 +0000

    Update apc.ps1

--- apc.ps1
+++ apc.ps1
@@ -2,8 +2,9 @@
 # Settings
 # =========================
 
-$sessionMinutes = 1 # 5
-$pauseSeconds = 0 # 5
+$sessionMinutes = 1
+$pauseSeconds = 10
+# $pauseSeconds = Get-Random -Minimum 5 -Maximum 11 # It's 11 instead of 10 because of exclusiveness
 
 # =========================
 # Choose sentence list
